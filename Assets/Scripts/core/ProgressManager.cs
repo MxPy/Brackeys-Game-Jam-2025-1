0f258f0b57a4403029cf88f345872f33ed73522f
Maximilian Patalas <maksymilianpatalas@gmail.com>
2025-02-17 22:14:07 +0100

    add demos and core

---
+++ Assets/Scripts/core/ProgressManager.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Numerics;
+using Unity.Collections;
+using UnityEngine;
+
+public class Item{
+      public Item(uint key, string name)
+    {
+        this.key = key;
+        this.name = name;
+    }
+    public uint key;
+    public string name;
+    public bool acquired = false;
+    
+    // Item references
+    public static List<Item> ITEMS = new()
+    {
+        new(0, "cat"),
+        new(1, "keys")
+    };
+}
+public class ProgressManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Debug.Log(Item.ITEMS[0].acquired);
+        // Item.ITEMS[0].acquired = true;
+        // Debug.Log(Item.ITEMS[0].acquired);
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
